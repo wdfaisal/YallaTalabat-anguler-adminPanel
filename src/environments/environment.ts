6ea683b9c7a9752261c21024a5ceaa7f53c9a3a3
Ahmed Faisal (Al-Sheikhe) <105755473+wdfaisal@users.noreply.github.com>
2026-06-11 00:27:58 +0100

    Update apiUrl to include trailing slash

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appName: 'FoodBite',
-  apiUrl: 'https://yallatalbat.site',
+  apiUrl: 'https://yallatalbat.site/',
   defaultCountryCode: '+61', // please append +
   version: '1.0.0'
 };
